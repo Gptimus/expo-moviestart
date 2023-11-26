63dc972f7534dd75ec0155e981a6c3505d658b11
Guerth Manzala <guerthmanzala@gmail.com>
2023-11-26 09:06:04 +0100

    drawer added

---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,36 @@
+import { colorTokens } from '@tamagui/themes';
+import Drawer from 'expo-router/drawer';
+import { Home, Star } from 'lucide-react-native';
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <Drawer
+      screenOptions={{
+        headerShown: true,
+        drawerHideStatusBarOnOpen: true,
+        drawerActiveBackgroundColor: colorTokens.dark.blue.blue7,
+        drawerActiveTintColor: '#fff',
+        drawerLabelStyle: { marginLeft: -20 },
+      }}>
+      <Drawer.Screen
+        name="home"
+        options={{
+          title: 'Moviestar',
+          headerShown: false,
+          drawerIcon: ({ color, size }) => <Home color={color} size={size} />,
+        }}
+      />
+      <Drawer.Screen
+        name="favorites"
+        options={{
+          title: 'My Favorites',
+          headerShown: false,
+          drawerIcon: ({ color, size }) => <Star color={color} size={size} />,
+        }}
+      />
+    </Drawer>
+  );
+};
+
+export default Layout;
